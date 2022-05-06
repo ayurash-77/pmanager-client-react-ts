6d3406b42f060e8393645bec14dcb8a87e9020ab
ayurash-77 <ayurash@me.com>
2022-05-06 11:44:26 +0300

    Upgrade to React 18.1.0

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './app/App'
 import { setupStore } from './store/store'
@@ -10,12 +10,22 @@ const store = setupStore()
 
 setupListeners(store.dispatch)
 
-ReactDOM.render(
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   </React.StrictMode>,
+//
+//   document.getElementById('root')
+// )
+
+const container = document.getElementById('root')
+const root = createRoot(container) // createRoot(container!) if you use TypeScript
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-
-  document.getElementById('root')
+  </React.StrictMode>
 )
